3b828bc57fa2233b321b97be8739934c0ec21594
Guilherme Mattos da Silva <guilhermems.pp@gmail.com>
2023-05-24 22:16:15 -0300

    Creating Form Section

---
+++ src/components/SectionForms/index.tsx
@@ -0,0 +1,50 @@
+import React, { memo } from 'react'
+
+import Config from 'Config'
+
+import H2Title from 'components/H2Title'
+
+import {
+  ButtonStyled,
+  DivBackground,
+  FormStyled,
+  InputStyled,
+  SectionStyled,
+  TextareaStyled,
+} from './style'
+
+const SectionForms: React.FC = () => {
+  return (
+    <SectionStyled id="contato">
+      <DivBackground className="py-4">
+        <H2Title title="Quer Falar Conosco?" color="white" />
+
+        <FormStyled action={Config.app.form} method="POST">
+          <InputStyled
+            type="text"
+            name="nome"
+            id="nome"
+            placeholder="Seu nome"
+            className="mt-2"
+            required
+          />
+          <InputStyled
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Seu e-mail"
+            required
+          />
+          <TextareaStyled
+            id="message"
+            name="message"
+            placeholder="Sua mensagem"
+          />
+          <ButtonStyled type="submit">Enviar</ButtonStyled>
+        </FormStyled>
+      </DivBackground>
+    </SectionStyled>
+  )
+}
+
+export default memo(SectionForms)
---
+++ src/components/SectionForms/style.ts
@@ -0,0 +1,75 @@
+import styled from 'styled-components'
+
+export const SectionStyled = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 10vh;
+  ::-webkit-input-placeholder {
+    color: white;
+  }
+`
+export const DivBackground = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  align-items: center;
+  background-color: #02acd9;
+  color: white;
+`
+export const FormStyled = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  align-items: center;
+  border-color: #00728f;
+`
+export const InputStyled = styled.input`
+  background: none;
+  color: white;
+  box-shadow: 0 0 0 0;
+  border: 0 none;
+  outline: 0;
+  border-bottom: 2px;
+  padding: 0 1rem 0 1rem;
+  border-style: solid;
+  margin-bottom: 2rem;
+  width: 50%;
+  @media (max-width: 425px) {
+    width: 80%;
+  }
+`
+export const TextareaStyled = styled.textarea`
+  background: none;
+  color: white;
+  box-shadow: 0 0 0 0;
+  border: 0 none;
+  outline: 0;
+  padding: 0.2rem 1rem 0.2rem 1rem;
+  border: 2px;
+  border-style: solid;
+  margin-bottom: 2rem;
+  margin-top: 0.4rem;
+  width: 50%;
+  min-height: 100px;
+  @media (max-width: 425px) {
+    width: 80%;
+  }
+`
+export const ButtonStyled = styled.button`
+  background: none;
+  color: white;
+  font-weight: 700;
+  box-shadow: 0 0 0 0;
+  border: 0 none;
+  padding: 0.5em 3em 0.5em 3em;
+  outline: 0;
+  background-color: #00728f;
+
+  :hover {
+    background-color: #004354;
+  }
+`
