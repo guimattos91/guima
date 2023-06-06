4ebfc402a9cab2e2e0898beab69671ade994915e
Guilherme Mattos da Silva <guilhermems.pp@gmail.com>
2023-06-06 18:20:10 -0300

    Adding Theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const TextWhite = styled.p`
+  padding: 0;
+  margin: 0;
+  color: white;
+`
+export const TextLightBlue = styled.p`
+  padding: 0;
+  margin: 0;
+  color: #02acd9;
+`
+export const TextBlack = styled.p`
+  padding: 0;
+  margin: 0;
+  color: #1d1d1d;
+`
